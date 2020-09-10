e975fc876cce8b0b9cf6b73f2ee3f66204067984
Anastasiia Lukina <anlukina24@gmail.com>
2020-09-11 01:14:58 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.58edaffc41ad9664059236acc9b397b1.js"
 );
 
 self.addEventListener('message', (event) => {
